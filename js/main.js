371aadf53848a3c30c9a1868d06fdbae60a29087
OleksiiKuz <61288803+OleksiiKuz@users.noreply.github.com>
2020-04-24 22:49:24 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,40 @@
+$(function(){
+   $('.header__burger').on('click', function (e) {
+      e.preventDefault;
+      $(this).toggleClass('header__burger-active')
+
+   });
+
+   $('.header__burger').on('click', function (e) {
+      e.preventDefault;
+      $('.header__navigation').slideToggle();
+
+   });
+
+   $('.header__burger').on('click', function (e) {
+      e.preventDefault;
+      $('.header__navigation').toggleClass('header__navigation-active')
+
+   });
+
+   $('a[href*="#"]').on('click', function (e) {
+      e.preventDefault();
+
+      $('html, body').animate({
+         scrollTop: $($(this).attr('href')).offset().top
+      }, 1000, 'linear');
+   });
+
+
+
+});
+
+$(document).ready(function () {
+   $('.promo__slider').slick({
+      dots: true,
+      vertical: true,
+      arrows: false,
+      dotsClass: 'promo__dots'
+  });
+});
+
